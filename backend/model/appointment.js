9737d318a4d88b9801c730f8aac39ddcc1b6a57f
william <emelifonwuw@gmail.com>
2025-03-23 19:54:44 +0100

    feat: add purpose and time fields to appointment model

--- backend/model/appointment.js
+++ backend/model/appointment.js
@@ -4,6 +4,8 @@ const appointmentSchema = new mongoose.Schema({
     patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     doctorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor' },
     medicalNotes: { type: String },
+    purpose: { type: String },
+    time: { type: String },
     date: Date,
     status: { type: String, default: 'Pending' }
 });
